378b759105c53bd2b6fdb4c3cbcc56eed7242875
patrick-angelos <angelosp07@gmail.com>
2021-02-17 11:34:58 +0200

    Move logic into helper files

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -6,4 +6,25 @@ module EventsHelper
   def add_event_path
     "/events/add/#{@event.id}"
   end
+
+  def attend_link
+    link_to 'Attend this event', add_event_path, method: :put if !attendee? && user_signed_in?
+  end
+
+  def attendees_names
+    names = ''
+    @event.attendees.each do |attendee|
+      names << (content_tag :p, (link_to attendee.username, attendee))
+    end
+    names.html_safe
+  end
+
+  def show_events(events)
+    events_names = ''
+    events.each do |event|
+      text = event.title
+      events_names << (content_tag :p, (link_to text, event))
+    end
+    events_names.html_safe
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,15 @@
 module UsersHelper
+  def show_links
+    links = ''
+    if user_signed_in?
+      text = 'Logged in as: '
+      links << (content_tag :p, text)
+      links << (content_tag :p, (link_to current_user.username, current_user))
+      links << (content_tag :p, (link_to 'Logout', users_log_out_path, method: :delete))
+      links << (content_tag :p, (link_to 'Login', users_sign_in_path))
+    else
+      links << (content_tag :p, (link_to 'Login', users_sign_in_path))
+    end
+    links.html_safe
+  end
 end
