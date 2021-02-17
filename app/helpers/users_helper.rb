cfa8de15ae52bedd7c0b460083cdc7b85e04cfba
patrick-angelos <angelosp07@gmail.com>
2021-02-17 11:36:52 +0200

    Fix linters

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,7 +6,6 @@ module UsersHelper
       links << (content_tag :p, text)
       links << (content_tag :p, (link_to current_user.username, current_user))
       links << (content_tag :p, (link_to 'Logout', users_log_out_path, method: :delete))
-      links << (content_tag :p, (link_to 'Login', users_sign_in_path))
     else
       links << (content_tag :p, (link_to 'Login', users_sign_in_path))
     end
